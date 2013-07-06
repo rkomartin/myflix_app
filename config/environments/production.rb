e165420a741fa37e6d7e24ea5d26a5405640757f
Robert Komartin <robert.komartin@gmail.com>
2013-07-06 10:54:10 +0300

    Fixing SMTP on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,10 +55,10 @@ Myflix::Application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
-    enable_starttls_auto: true }
+    enable_starttls_auto: true,
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'] }
   config.action_mailer.default_url_options = { host: 'http://rk-myflix.herokuapp.com' }
 
   # Enable threaded mode
