e1f698d5c61602b9a2c555430fb0fffe78ebb9cd
rkomartin <ubuntu@ip-10-138-34-123.ec2.internal>
2013-07-06 06:08:37 +0000

    SMTP corrected

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Myflix::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
+    port:                 465,
     domain:               'rk-myflix.herokuapp.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
