c038f7e6ac617f69b23a22aecbe144e111a7544a
Robert Komartin <robert.komartin@gmail.com>
2013-07-16 08:19:12 +0300

    Feature spec for user invites friend

---
+++ spec/features/user_invites_friend.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+feature "User invites friend" do
+  scenario "User successfully invites friend and invitation is accepted" do
+    alice = Fabricate(:user)
+    sign_in(alice)
+
+    visit new_invitation_path
+    fill_in "Friend's Name", with: "John Doe"
+    fill_in "Friend's Email Address", with: "john@example.com"
+    fill_in "Message", with: "Please join!"
+    click_button "Send Invitation"
+
+    open_email "john@example.com"
+    current_email.click_link "Accept this invitation"
+
+    fill_in "Password", with: "password"
+    fill_in "Full Name", with: "John Doe"
+    click_button "Sign Up"
+
+    fill_in "Email Address", with: "john@example.com"
+    fill_in "Password", with: "password"
+    click_button "Sign in"
+
+    expect(page).to have_content "John Doe"
+
+    clear_email
+end
--- spec/features/user_resets_password_spec.rb
+++ spec/features/user_resets_password_spec.rb
@@ -20,5 +20,7 @@ feature 'User resets password' do
     click_button "Sign in"
 
     expect(page).to have_content("Welcome, #{alice.full_name}")
+
+    clear_email
   end
 end
