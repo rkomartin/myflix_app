85d0e0f434035fe82780b7edb4b203e4a818a5ee
Robert Komartin <robert.komartin@gmail.com>
2013-07-21 21:38:26 +0300

    Admin role

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -9,8 +9,8 @@ describe Genre do
 
   it "has many videos" do
     genre = Genre.create(category: "Comedy")
-    video1 = Video.create(title: "Monk", description: "Monk is Monk", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg")
-    video2 = Video.create(title: "Family Guy", description: "Well, a guy with a family", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/monk_large.jpg")
+    video1 = Video.create(title: "Monk", description: "Monk is Monk", small_cover: File.open(File.join(Rails.root, "/public/tmp/monk.jpg")), large_cover: File.open(File.join(Rails.root, "/public/tmp/monk_large.jpg")))
+    video2 = Video.create(title: "Family Guy", description: "Well, a guy with a family", small_cover: File.open(File.join(Rails.root, "/public/tmp/monk.jpg")), large_cover: File.open(File.join(Rails.root, "/public/tmp/monk_large.jpg")))
     genre.videos << video1
     genre.videos << video2
     genre.videos.size.should == 2
